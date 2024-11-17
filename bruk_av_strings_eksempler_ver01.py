d440404c5b08e843926adbfd87c9204d42d0912a
Oeselsvik <oeselsvik@outlook.com>
2024-11-17 20:35:13 +0100

    forelesning 3

---
+++ bruk_av_strings_eksempler_ver01.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # eksempler på bruk av strings
+# 
+
+# In[15]:
+
+
+år = int(input("Skriv inn året"))
+
+print("Nest år er det", år + 1)
+print("Om 10 år er det", år +10)
+
+get_ipython().run_line_magic('whos', '')
+
+
+# In[41]:
+
+
+get_ipython().run_line_magic('reset', '-f')
+a = 12
+b = int(input("12"))
+print(a + b)
+
+get_ipython().run_line_magic('whos', '')
+
+
+# In[47]:
+
+
+get_ipython().run_line_magic('reset', '-f')
+
+a = "abcd"
+b = "1234"
+c = "34"
+print(b+c)
+
+e = "snart "
+f = "mandag"
+print(e+f)
+
+get_ipython().run_line_magic('whos', '')
+
+
+# In[ ]:
+
+
+
+
