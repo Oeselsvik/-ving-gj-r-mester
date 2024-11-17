63a78203fff62c205de182ca02de5e94887c704f
Oeselsvik <oeselsvik@outlook.com>
2024-11-17 16:33:16 +0100

    Add files via upload

---
+++ variabel som endres, eksempel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # eksempel som viser at variablen endrer verdi undervei
+# 
+#     
+
+# In[28]:
+
+
+konto = 480000
+
+innskudd = int(input("hvor mye tenker du å sette inn?"))
+
+konto = konto + innskudd 
+
+print(" nå har du kr", konto, "på din sparekonto")
+
+
+
+# In[ ]:
+
+
+
+
