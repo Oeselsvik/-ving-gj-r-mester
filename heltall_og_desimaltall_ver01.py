b870dc757c2fc55bc8e2b572232150142c6d63ee
Oeselsvik <oeselsvik@outlook.com>
2024-11-17 19:45:57 +0100

    test
    her kan jeg skrive noe utfyllende

---
+++ heltall_og_desimaltall_ver01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #eksempel på heltall og desimaltall
+
+# In[24]:
+
+
+a = 2
+b = 3.4
+
+c = 7 + 10**(-14)
+print(c)
+# over opphøyd i 15(-15) vil vises som nærmeste heltall. Kan ikke ha uendelig siffer pga kapasitet i programmet
+get_ipython().run_line_magic('whos', '')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
